14fb48c32b0fa7f797f3c8fd6a7b534bfee21292
Mukesh <mukesh2305b@gmail.com>
2026-09-12 22:31:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-// main.jsx or index.jsx
-import { Analytics } from "@vercel/analytics/react"
+import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; // Ensure the path is correct
-import "./index.css"; // Add styles if required
+import App from "./App";
+import { MusicProvider } from "./context/MusicContext";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-
-    <App  />
-    
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <MusicProvider>
+      <App />
+    </MusicProvider>
+  </React.StrictMode>
 );
